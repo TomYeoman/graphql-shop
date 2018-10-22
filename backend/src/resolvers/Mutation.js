1b252f559f53bc988695e7b3c9b4b4520c2d1ed1
Tom Yeoman <tom.yeoman@live.co.uk>
2018-10-22 21:46:17 +0100

    Add code to perform local graphql resolver resolution

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+    /**
+     * 
+     * @param {*} parent 
+     * @param {*} args 
+     * @param {*} context 
+     * @param {*} info 
+     */
+    createDog(parent, args, context, info) {
+        console.log(args)
+        global.dogs = global.dogs || [];
+        const newDog = {name : args.name};
+        global.dogs.push(newDog)
+        return newDog;
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,21 @@
-const Query = {};
+const Query = {
+    /**
+     * This can retrieve from anywhere, rest API, locally, or in our case prisma
+     * @param {*} parent 
+     * @param {*} args 
+     * @param {*} context - Do we need headers, cookies etc? also covers DB
+     * @param {*} info - 
+     */
+    dogs(parent, args, context, info) {
+        return [
+            {name : 'Sam'},
+            {name : 'Archie'},
+            {name : 'Charlie'},
+        ]
+    },
+    globalDogs(parent, args, context, info) {
+        return global.dogs || []
+    }
+};
 
 module.exports = Query;
