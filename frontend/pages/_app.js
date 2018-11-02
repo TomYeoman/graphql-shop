3ef2cd9f77b6710435812d34b6926f81b8a41b97
Tom Yeoman <tom.yeoman@live.co.uk>
2018-11-02 20:48:45 +0100

    Update app.js wired up to apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,23 +1,51 @@
 import App, {Container} from 'next/app'
-
-import React, { Component } from 'react'
 import Page from '../components/Page'
 import Meta from '../components/Meta'
 
-export default class MyCustomApp extends App {
+import {ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData'
+
+class MyApp extends App {
+
+  // When we go to different pages (e.g items?page=2 ) we're going to need to surface those values for the client ( Used to be done automatically for us )
+  // This is a special next.js lifecycle method ( Runs before the first render happens ), which exposes some props for us
+
+  // If we go to next.js / apollo.js we will find this snippet for this
+  static async getInitialProps({Component, ctx }) {
+    let pageProps = {}
+
+    // This crawls every single page, fetches all the data (queries / mutations ) and returns it for us. E.g if we have a basket, list of items etc this pre-renders for us (this is just needed for SSR apps)
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps;
+    }
+
+    // This exposes the query to the user
+    pageProps.query = ctx.query;
+    // This exposes it via props
+    return { pageProps }
+  }
+
   render() {
 
-    // Routed component is passed as props from main hidden <App/>
-    const { Component } = this.props;
+    
+    // Component - Routed component is passed as props from main hidden <App/>
+    // Apollo - ApolloProvider needs a client to us
+    const { Component, apollo, pageProps } = this.props;
+
     return (
       <div>
           <Container>
-            <Meta/>
-            <Page>
-              <Component/>
-            </Page>
+            <ApolloProvider client={this.props.apollo}>
+              <Meta/>
+              <Page>
+                <Component/>
+              </Page>
+            </ApolloProvider>
           </Container>
       </div>
     )
   }
 }
+
+// This exposes the apollo prop for us to use within the ApolloProvider
+export default withData(MyApp)
