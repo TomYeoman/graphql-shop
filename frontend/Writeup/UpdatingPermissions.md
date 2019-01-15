7f9cb6e7351aedcd7987f4ea11f3a36b592ba853
Tom Yeoman <tom.yeoman@live.co.uk>
2019-01-15 20:03:29 +0000

    Update UI to use state ( #35 )

---
+++ frontend/Writeup/UpdatingPermissions.md
@@ -0,0 +1,2 @@
+Create `seed` data within Permissions within the UserPermissions component + wire up and onChange handler to updat epermissions
+
--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Error from './ErrorMessage';
@@ -28,9 +29,13 @@ const PermissionsPage = props => (
   <Query query={ALL_USERS_QUERY}>
     {({ data, loading, error }) => {
       console.log(data);
+      if (loading) {
+        return <div>Loading</div>;
+      }
       if (error) {
         return <Error error={error} />;
       }
+
       return (
         <div>
           Manage Permissions
@@ -40,13 +45,13 @@ const PermissionsPage = props => (
                 <th>Name</th>
                 <th>Email</th>
                 {possiblePermissions.map(permission => (
-                  <th>{permission}</th>
+                  <th key={permission}>{permission}</th>
                 ))}
               </tr>
             </thead>
             <tbody>
               {data.users.map(user => (
-                <User user={user} />
+                <UserPermissions key={user.id} user={user} />
               ))}
             </tbody>
           </Table>
@@ -56,7 +61,31 @@ const PermissionsPage = props => (
   </Query>
 );
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }),
+  };
+
+  state = {
+    permissions: this.props.user.permissions,
+  };
+
+  handleChange = e => {
+    const { value, checked } = e.target;
+    const currentPermissions = [...this.state.permissions];
+
+    this.setState({
+      permissions: checked
+        ? [...currentPermissions, value]
+        : currentPermissions.filter(item => item !== value),
+    });
+  };
+
   render() {
     console.log(this.props);
     const { name, email, id } = this.props.user;
@@ -65,9 +94,15 @@ class User extends React.Component {
         <td>{name}</td>
         <td>{email}</td>
         {possiblePermissions.map(permission => (
-          <td>
+          <td key={permission}>
             <label htmlFor={`${id}-permission-${permission}`}>
-              <input type="checkbox" />
+              <input
+                id={`${id}-permission-${permission}`}
+                checked={this.state.permissions.includes(permission)}
+                type="checkbox"
+                onChange={this.handleChange}
+                value={permission}
+              />
             </label>
           </td>
         ))}
